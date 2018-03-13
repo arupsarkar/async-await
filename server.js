f3217e628256ec9fc1a3c4bc7a1b516fffac04ec
arupsarkar <sarkar.arup@gmail.com>
2018-03-12 23:25:47 -0400

    Async and await implementation

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+const asyncMiddleware = fn =>
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next))
+      .catch(next);
+  };
+
+
+app.get('/users/:id', asyncMiddleware(async (req, res, next) => {
+    /*
+      if there is an error thrown in getUserFromDb, asyncMiddleware
+      will pass it to next() and express will handle the error;
+    */
+    const user = await getUserFromDb({ id: req.params.id })
+    res.json(user);
+}));
+
+
+
+var getUserFromDb = function(userId){
+  if(userId == 1){
+    return "John Smith"
+  }else if(userId == 2){
+    return "Jane Smith"
+  }else if(userId == 3){
+    return "Mark Cecillio"
+  }else{
+    return "Julio Tally"
+  }
+
+}
+
+// Serve your app
+console.log('Served: http://localhost:' + port);
+app.listen(port);
