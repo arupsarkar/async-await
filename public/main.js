a05f366e9c237f3ecb888ab5cc2b0645ce890059
arupsarkar <sarkar.arup@gmail.com>
2018-03-14 15:06:55 -0400

    Added Query and Create CRUD operation

---
+++ public/main.js
@@ -0,0 +1,95 @@
+var app = angular.module("myApp", ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'home.html',
+      controller: 'StudentCtrl'
+    })
+    .when('/viewStudents', {
+      templateUrl: 'viewStudents.html',
+      controller: 'StudentCtrl'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+    //use the HTML5 History API
+    // $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+});
+
+
+// angular.module('StudentController', [])
+
+app.controller('StudentCtrl', function($scope, StudentFactory) {
+    // $scope.students = [
+    //   {name: 'Mark Waugh', city:'New York'},
+    //   {name: 'Steve Jonathan', city:'London'},
+    //   {name: 'John Marcus', city:'Paris'}
+    // ];
+    $scope.message = "Click on the hyper link to view the students list.";
+    var promise;
+    promise = StudentFactory.getAll();
+    promise.then(function(successPayload){
+      console.log('client data', JSON.stringify(successPayload.data));
+      $scope.students = successPayload.data;
+    });
+
+    $scope.createStudent = function(){
+      var studentData = $scope.Student;
+      console.log('Controller studentData :', JSON.stringify(studentData));
+      promise = StudentFactory.create(studentData);
+      promise.then(function(successPayload){
+        $scope.students = successPayload.data;
+      }, function(errorPayload){
+        console.log('Error-createStudent : ', error);
+      });
+    };
+
+
+});
+
+// angular.module('StudentService', [])
+
+  // super simple service
+  // each function returns a promise object
+app.factory('StudentFactory', ['$http',function($http) {
+    return {
+      getAll: function() {
+        console.log('StudentFactory - getAll');
+        return $http.get('/api/students');
+      },
+      get: function(id) {
+        return $http.get('/api/students/' + id);
+      },
+      create: function(studentData) {
+        console.log('Factory studentData :', JSON.stringify(studentData));
+        return $http.post('/api/students/' + JSON.stringify(studentData));
+      },
+      delete: function(id) {
+        return $http.delete('/api/students/' + id);
+      }
+    }
+}]);
+
+
+// angular.module('todoService', [])
+
+//   // super simple service
+//   // each function returns a promise object
+//   .factory('Todos', ['$http',function($http) {
+//     return {
+//       get : function() {
+//         return $http.get('/api/todos');
+//       },
+//       create : function(todoData) {
+//         return $http.post('/api/todos', todoData);
+//       },
+//       delete : function(id) {
+//         return $http.delete('/api/todos/' + id);
+//       }
+//     }
+//   }]);
--- server.js
+++ server.js
@@ -5,17 +5,17 @@ var port = process.env.PORT || 8080;
 // Serve static files
 app.use(express.static(__dirname + '/public'));
 
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
+var students = {};
 
 const asyncMiddleware = fn =>
   (req, res, next) => {
     Promise.resolve(fn(req, res, next))
       .catch(next);
   };
+
+app.get('/', asyncMiddleware(async (req, res, next) => {
+  res.send('Hello World!')
+}));
 
 
 app.get('/users/:id', asyncMiddleware(async (req, res, next) => {
@@ -24,11 +24,47 @@ app.get('/users/:id', asyncMiddleware(async (req, res, next) => {
       will pass it to next() and express will handle the error;
     */
     //console.log("INFO : ", JSON.stringify(req))
-    const user = await getUserFromDb({ id: req.params.id })
+    const user = await getUserFromDb({ id: req.params.id });
     res.json(user);
 }));
 
 
+app.get('/api/students', asyncMiddleware(async (req, res, next) => {
+  console.log('Inside server /api/students');
+  const studentData = await getStudents();
+  console.log('Student data : ', JSON.stringify(studentData));
+  res.json(studentData);
+}));
+
+app.post('/api/students/:studentData', asyncMiddleware(async (req, res, next) => {
+  var rawData = req.params.studentData;
+  var obj = JSON.parse(rawData);
+  console.log('POST : create Students - Start');
+  console.log('POST : create Students - req ', rawData);
+  console.log('POST : create Students - studentData name ', obj.name);
+  // students.push({id: obj.id, name: obj.name, city: obj.city});
+  const studentData = await saveStudent(rawData);
+  console.log(JSON.stringify(studentData))
+  res.json(studentData);
+  console.log('POST : create Students - End');
+}));
+
+
+var saveStudent = function(studentData){
+  var rawData = studentData;
+  var obj = JSON.parse(rawData);
+  students.push({id: obj.id, name: obj.name, city: obj.city});
+  return students;
+}
+
+var getStudents = function(){
+    students = [
+      {id: 1, name: 'Mark Waugh', city:'New York'},
+      {id: 2, name: 'Steve Jonathan', city:'London'},
+      {id: 3, name: 'John Marcus', city:'Paris'}
+    ];
+    return students;
+}
 
 var getUserFromDb = function(userId){
   console.log(userId)
