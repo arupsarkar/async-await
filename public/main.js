22baa682c31bfdfd79827fb392853f1918e95d7e
arupsarkar <sarkar.arup@gmail.com>
2018-03-26 11:56:20 -0400

    console.log added

--- public/main.js
+++ public/main.js
@@ -97,12 +97,13 @@ app.controller('AuthCtrl', function($scope, $location, $cookies, $http, Salesfor
         host: $location.host(),
         port: $location.port() ? ':' + $location.port() : ''
     });
-
+    console.log(url);
+    console.log(redirectUri);
     // Client IDs for dev and qa are configured in the ProductQA org
     var clientMap = {
         'async-await.herokuapp.com': '3MVG9A2kN3Bn17huTvg2gF_6Kh1ATXcOoETtZlqlblwhrO4SEKFjtsUbZXvSydokyfqjlkcFI95bKXoa0n54U'
     };
-
+    console.log(clientMap[$location.host()]);
     window.location.href = url.supplant({
         'client_id': clientMap[$location.host()],
         'redirect_uri': redirectUri
