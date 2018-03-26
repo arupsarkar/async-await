bc7bde5167e414d5d6e82015842509c21bd2324b
arupsarkar <sarkar.arup@gmail.com>
2018-03-26 01:36:44 -0400

    Enabled CORS

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ var app = express();
 var bodyParser= require('body-parser');
 var port = process.env.PORT || 8080;
 
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 // Serve static files
+app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
