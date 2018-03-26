2f84324e4cc6f4a1d2c559a8501e7a700f6442bf
arupsarkar <sarkar.arup@gmail.com>
2018-03-26 11:39:43 -0400

    Adding OAuth2.0

--- public/main.js
+++ public/main.js
@@ -86,16 +86,32 @@ app.controller('StudentCtrl', function($scope, StudentFactory, $location) {
 });
 
 // salesforce authetication OAuth2.0 flow
-app.controller('AuthCtrl', function($scope, SalesforceConnectionFactory) {
-  var promise = SalesforceConnectionFactory.salesforceAuth();
-  promise.then(function(successPayload){
-    console.log('ctrl success', successPayload.data);
-  }, function(errorPayload){
-    console.log('ctrl error', errorPayload.data);
-  })
+app.controller('AuthCtrl', function($$scope, $location, $cookies, $http, SalesforceConnectionFactory) {
+    // Delete the local storage data
+    $cookies.put("SF_ACCESS_TOKEN", '');
+    $cookies.put("SF_INSTANCE_URL", '');
+    // Redirect to the page
+    var url = 'https://login.salesforce.com/services/oauth2/authorize?response_type=token&client_id={client_id}&redirect_uri={redirect_uri}';
+    var redirectUri = '{protocol}://{host}{port}/auth/callback'.supplant({
+        protocol: $location.protocol(),
+        host: $location.host(),
+        port: $location.port() ? ':' + $location.port() : ''
+    });
+
+    // Client IDs for dev and qa are configured in the ProductQA org
+    var clientMap = {
+        'async-await.herokuapp.com': '3MVG9A2kN3Bn17huTvg2gF_6Kh1ATXcOoETtZlqlblwhrO4SEKFjtsUbZXvSydokyfqjlkcFI95bKXoa0n54U'
+    };
+
+    window.location.href = url.supplant({
+        'client_id': clientMap[$location.host()],
+        'redirect_uri': redirectUri
+    });
+
 });
 
-app.controller('AuthCallbackCtrl', function($scope, SalesforceConnectionFactory) {
+app.controller('AuthCallbackCtrl', function($scope, $location, $cookies) {
+  console.log($location.hash.split('&'));
 });
 
 
