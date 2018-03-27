ac1fc462af83e037342d65adde262fe73e6a6f06
arupsarkar <sarkar.arup@gmail.com>
2018-03-26 23:50:00 -0400

    Added cors

--- server.js
+++ server.js
@@ -3,26 +3,33 @@ var app = express();
 var bodyParser= require('body-parser');
 var port = process.env.PORT || 8080;
 var jsforce = require('jsforce');
+var cors = require('cors');
 
 var allowCrossDomain = function(req, res, next) {
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // // intercept OPTIONS method
+    // if ('OPTIONS' == req.method) {
+    //   res.send(200);
+    // }
+    // else {
+    //   next();
+    // }
+
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
+    next();
 };
 
 
 
 
 // Serve static files
-app.use(allowCrossDomain);
+app.use(cors());
+//app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
