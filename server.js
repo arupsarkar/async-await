3c41a43fb6c4022b7c1420183239d248644e7ed8
arupsarkar <sarkar.arup@gmail.com>
2018-03-28 16:29:20 -0400

    Added CSP

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.header('Content-Security-Policy', 'unsafe-inline');
+    res.header('Content-Security-Policy', 'nonce');
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
       res.send(200);
