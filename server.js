b769ec3f93f8c84677337ed6dac9730dd632088f
arupsarkar <sarkar.arup@gmail.com>
2018-03-28 14:54:36 -0400

    URL redirection

--- server.js
+++ server.js
@@ -71,6 +71,8 @@ app.get('/callback', asyncMiddleware(async (req, res, next) => {
   io.on('connection', function(socket){
     socket.emit('community', {data: 'community payload'});
   });
+  var landingUrl = "https://rc-ca-developer-edition.na54.force.com/s";
+  window.location.href = landingUrl;
   res.send('success');
   // var conn = new jsforce.Connection({ oauth2 : oauth2 });
   // var code = req.param('code');
